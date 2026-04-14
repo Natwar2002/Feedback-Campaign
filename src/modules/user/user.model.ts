f39857cefc59c1c9988a254d82d30143652bfd0d
natwarpatidar-cloud <natwar.patidar@gammaedge.io>
2026-04-14 16:09:55 +0530

    feat: add user schema

---
+++ src/modules/user/user.model.ts
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    avatar: {
+        type: String,
+        required: false
+    },
+    platformRole: {
+        enum: ["SuperAdmin", "User"],
+        required: true,
+        default: "User"
+    },
+    externalUserId: {
+        type: String,
+        required: true
+    },
+}, {
+    timestamps: true
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
